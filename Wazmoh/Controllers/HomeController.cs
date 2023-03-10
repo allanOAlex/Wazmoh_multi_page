d2085b7cd48a415ebde7d3da2a57088da7a51e67
allanOAlex <allan.alex0803@gmail.com>
2023-03-10 16:16:40 +0300

    Done social media icons functionality, pending the exact user pages. Currently the default social medial platform pages are loaded

--- Wazmoh/Controllers/HomeController.cs
+++ Wazmoh/Controllers/HomeController.cs
@@ -49,6 +49,54 @@ namespace Wazmoh.Controllers
             return View();
         }
 
+        public IActionResult WhatsApp()
+        {
+            string url = "https://web.whatsapp.com/";
+            return Redirect(url);
+        }
+
+        public IActionResult Facebook()
+        {
+            string url = "https://www.facebook.com/";
+            return Redirect(url);
+        }
+
+        public IActionResult Twitter()
+        {
+            string url = "https://twitter.com/";
+            return Redirect(url);
+        }
+
+        public IActionResult Instagram()
+        {
+            string url = "https://www.instagram.com/";
+            return Redirect(url);
+        }
+
+        public IActionResult LinkedIn()
+        {
+            string url = "https://www.linkedin.com/";
+            return Redirect(url);
+        }
+
+        public IActionResult Email()
+        {
+            string url = "mailto:allan.alex0803@gmail.com";
+            return Redirect(url);
+        }
+
+        public IActionResult GitHub()
+        {
+            string url = "https://github.com/allanOAlex";
+            return Redirect(url);
+        }
+
+        public IActionResult AuthorProfile()
+        {
+            string url = "";
+            return Redirect(url);
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
